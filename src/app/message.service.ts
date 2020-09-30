d6adc6345a42a88ce1b80524717d9404d37a3649
Alex Muravya <kyberorg@yadev.eu>
2020-09-30 11:36:26 +0300

    Dynamic API Root

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {StubMessage} from './stubMessage';
+import {environment} from '../environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
-  private apiUrl = 'http://localhost:8080/stub';
+  private apiUrl = environment.apiRoot + '/stub';
 
   constructor( private http: HttpClient) { }
 
