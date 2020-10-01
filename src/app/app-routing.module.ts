51d15bd6f128f0ceea19b0df313aa857e189cebc
Alex Muravya <kyberorg@yadev.eu>
2020-10-01 17:01:25 +0300

    Navigation View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {InfoPageComponent} from './components/info-page/info-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: HomePageComponent},
+  {path: 'info', component: InfoPageComponent},
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: '**', component: InfoPageComponent} // TODO WildCard
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+  private pMobileQueryListener: () => void;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.pMobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.pMobileQueryListener);
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.pMobileQueryListener);
+  }
 
 }
