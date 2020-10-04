db91267dc59b71dfc3d264a819f09af7a5f95162
Alex Muravya <kyberorg@yadev.eu>
2020-10-04 15:50:51 +0300

    Code structured

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {StubMessage} from '../../stubMessage';
-import {MessageService} from '../../message.service';
+import {Component, OnInit} from '@angular/core';
+import {StubMessage} from '../../classes/stubMessage';
+import {MessageService} from '../../services/message.service';
 
 @Component({
   selector: 'app-home-page',
@@ -10,7 +10,8 @@ import {MessageService} from '../../message.service';
 export class HomePageComponent implements OnInit {
   stubMessage: StubMessage;
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {
+  }
 
   ngOnInit(): void {
     this.messageService.getStubMessage().subscribe((data: StubMessage) => {
